0395ddcbe3b6f67c173aacaaf2819edc3622f571
Max <mn27304@gmail.com>
2020-04-02 18:53:10 +0200

    Optimization ready

---
+++ dist/js/tinyscript.js
@@ -0,0 +1,14 @@
+/* var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false
+    
+  }); */
+  //controlsText	(Text | Markup) Array	Default: ["prev", "next"].
+/* Text or markup in the prev/next buttons. */
+
+
+
+
+  // for Options  https://github.com/ganlanyuan/tiny-slider#install
